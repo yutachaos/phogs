d2ebfff7ab36f1e002de3f664474b829acb65b1c
y_kimura <y_kimura@yumemi.co.jp>
2016-09-03 16:11:56 +0900

    layout変更

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,41 +1,7 @@
 class SearchesController < ApplicationController
-    @title = 'phogs'
+    @title = 'teens'
     def index
       @searches = Search.all.page(params[:page]).per(3).order("created_at DESC")
-      @title = 'phogs'
-    end
-
-    def map
-      @searches = Search.all
-      @hash = Gmaps4rails.build_markers(@searches) do |search, marker|
-               marker.lat search.latitude
-               marker.lng search.longitude
-               marker.infowindow search.full_location
-               marker.json({title: search.location})
-      end
-      @title = 'phogs'
-    end
-    def get_location
-      require 'geocoder'
-      @formatted_address = ''
-      @keywords_address    = ''
-      @get_status = '現在位置取得'
-      @lat = params[:lat]
-      @lon = params[:lon]
-      if !@lat.nil? && !@lon.nil? then
-        begin
-          Geocoder.configure(:language  => :ja)
-          formatted_point = @lat.to_s + ',' + @lon.to_s
-          searched_data = Geocoder.search(formatted_point)
-          parseSearchedData(searched_data)
-          @get_status = '現在位置取得完了'
-        rescue Exception => e
-          @get_status = '現在位置取得失敗'
-          @get_status = @formatted_address
-          p e.message
-          p "geo searched exception catch!"
-        end
-     end
     end
 
     def create
@@ -61,25 +27,6 @@ class SearchesController < ApplicationController
     private
     def search_params
       params.permit(:name)
-    end
-
-    def parseSearchedData (searched_data)
-      keywords_arr = []
-      if !searched_data[0].data.nil? then
-          @formatted_address = searched_data[0].data["formatted_address"]
-          searched_data[0].data['address_components'].each do |address_component|
-            types = address_component['types']
-            if    types.include?('administrative_area_level_1')\
-               || types.include?('ward')\
-               || types.include?('locality')\
-               || types.include?('sublocality_level_1')
-               then
-              keywords_arr.push(address_component['long_name'])
-            end
-         end
-         keywords_arr.reverse!
-         @keywords_address = keywords_arr.join('')
-      end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
   root 'searches#index'
-  # resources 'searches' do
-  #   collection do
-  #     get 'get_location'
-  #   end
-  # end
-  get  'map',  to: 'searches#map'
-  get  'searches/get_location',  to: 'searches#get_location'
-  post 'result',   to: 'finds#result'
-  get  'show/:id', to: 'finds#show'
+  post 'result',   to: 'searches#result'
 end
